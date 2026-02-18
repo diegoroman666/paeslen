f6682d759e6e48eff4728c9483d5071c18f44dac
diegoroman666 <25reid88@gmail.com>
2026-02-17 21:38:56 -0300

    avanceee

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import "./styles/app.css";
+import "./styles/duolingo.css";
+import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -11,3 +13,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
+
